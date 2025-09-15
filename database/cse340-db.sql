3600183fca844fbf9fc8092e6d67a41f286de8e4
MichaelKazembe <michaelkazembe@gmail.com>
2025-09-15 16:54:43 +0200

    Update account_type ownership comment for clarity

--- database/cse340-db.sql
+++ database/cse340-db.sql
@@ -4,7 +4,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340;
+    OWNER TO cse340; -- change database  to newly created database in render.com
 
 -- Table structure for table 'classification'
 
