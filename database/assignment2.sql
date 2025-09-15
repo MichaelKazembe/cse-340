69b43438ccefd0f83d2a7480a5d484cad69b2534
MichaelKazembe <michaelkazembe@gmail.com>
2025-09-15 14:33:15 +0200

    Add SQL query to retrieve inventory make and model with classification for "Sport" category

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -43,3 +43,19 @@ SET
   inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
 WHERE
   inv_id = 10;
+
+-- 5. Use an Inner Join to select the make and model fields from the inventory table
+    -- and the classification name field from the classification table for inventory items
+    -- that belong to the "Sport" category.
+
+SELECT 
+    inv_make,
+    inv_model,
+    classification_name
+FROM 
+    public.inventory
+INNER JOIN 
+    public.classification
+    ON public.inventory.classification_id = public.classification.classification_id
+WHERE
+    classification_name = 'Sport'; 
