76a49327a7368e3014490dbbdb0a0a1a569021de
MichaelKazembe <michaelkazembe@gmail.com>
2025-09-18 23:03:07 +0200

    Add utility function to construct navigation HTML list from classifications

---
+++ utilities/index.js
@@ -0,0 +1,28 @@
+const invModel = require("../models/inventory-model");
+const Util = {}; // container for all utility functions
+
+/* ************************
+ * Constructs the nav HTML unordered list
+ ************************** */
+Util.getNav = async function (req, res, next) {
+  let data = await invModel.getClassifications(); // get the array of classifications
+  let list = "<ul>"; // start the list
+  list += '<li><a href="/" title="Home Page">Home</a></li>';
+  data.rows.forEach((row) => {
+    // for each classification row
+    list += "<li>"; // open a list item
+    list +=
+      '<a href="/inv/type/' +
+      row.classification_id +
+      '" title="See our inventory of ' +
+      row.classification_name +
+      ' vehicles">' +
+      row.classification_name +
+      "</a>";
+    list += "</li>"; // close the list item
+  });
+  list += "</ul>"; // close the list
+  return list;
+};
+
+module.exports = Util;
