a1d1e99a52cbad83b1b229ee2945af5a875bf683
MichaelKazembe <michaelkazembe@gmail.com>
2025-09-18 22:39:27 +0200

    Fix typo in baseController.js and improve comments for clarity

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,9 +1,9 @@
 const utilities = require("../utilities/");
-const baseController = {};
-
+const baseController = {}; // Initialize an empty object to hold controller methods
+// Define a method to build the home page
 baseController.buildHome = async function (req, res) {
   const nav = await utilities.getNav(); // Get the navigation data
   res.render("index", { title: "Home", nav }); // Render the index view with title and navigation data
 };
 
-modile.exports = baseController;
+module.exports = baseController;
