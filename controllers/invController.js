a6e68040c90824b56804a9a97fae3f2bddf0aac4
MichaelKazembe <michaelkazembe@gmail.com>
2025-09-20 21:52:04 +0200

    Add inventory controller to handle classification-based inventory views

---
+++ controllers/invController.js
@@ -0,0 +1,20 @@
+const invModel = require("../models/invModel");
+const utilities = require("../utilities/");
+
+const invCont = {}; // Initialize an empty object to hold controller methods
+
+/* ***************************
+ *  Build inventory by classification view
+ * ************************** */
+invCont.buildByClassificationId = async function (req, res, next) {
+  const classification_id = req.params.classification_id; // Get classification ID from the URL(from inventoryRoute.js)
+  const data = await invModel.getInventoryByClassificationId(classification_id);
+  const grid = await utilities.buildClassificationGrid(data); // Build the inventory grid
+  let nav = await utilities.getNav();
+  const className = data[0].classification_name; // Get classification name for the title
+  res.render("./inventory/classification", {
+    title: className + " vehicles",
+    nav,
+    grid,
+  });
+};
