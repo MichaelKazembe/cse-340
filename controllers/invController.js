9855b111d4dfa04085f1932090df87c2e183574a
MichaelKazembe <michaelkazembe@gmail.com>
2025-09-22 17:56:52 +0200

    Add inventory detail view controller method to render vehicle details

--- controllers/invController.js
+++ controllers/invController.js
@@ -26,6 +26,14 @@ invCont.buildDetailView = async function (req, res, next){
   const inv_id = req.params.invId; // Get inventory ID from the URL
   const data = await invModel.getInventoryById(inv_id); // Fetch inventory data based on inventory ID
   console.table(data); // Log the data for debugging
+  const detail = await utilities.buildDetailViewHtml(data); // Build the inventory detail HTML
+  let nav = await utilities.getNav(); // Get navigation HTML
+  const vehicleName = data[0].inv_make; // Get vehicle name for the title
+  res.render("./inventory/detail", {
+    title: vehicleName,
+    nav,
+    detail,
+  })
 };
 
 module.exports = invCont; // Export the controller object to be used in routes
