3aa744c3918e086c147bf34956d348828e73e8f3
MichaelKazembe <michaelkazembe@gmail.com>
2025-09-26 18:19:10 +0200

    Add flash message support to home page and adjust index view formatting

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -3,6 +3,7 @@ const baseController = {}; // Initialize an empty object to hold controller meth
 // Define a method to build the home page
 baseController.buildHome = async function (req, res) {
   const nav = await utilities.getNav(); // Get the navigation data
+  req.flash("notice", "This is a flash message."); // Set a flash message
   res.render("index", { title: "Home", nav }); // Render the index view with title and navigation data
 };
 
