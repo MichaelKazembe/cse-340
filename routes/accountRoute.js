5e38daae27aece9e2cfe4a42caa4682800309568
MichaelKazembe <michaelkazembe@gmail.com>
2025-09-26 21:01:14 +0200

    Refactor account routes and update head partial for favicon

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,10 +4,10 @@ const router = new express.Router();
 const accountController = require("../controllers/accountController");
 const utilities = require("../utilities/");
 
+// Route to build the login view
+router.get("/login", accountController.buildLogin);
+
 // Route to build the registration view
-router.get(
-  "/login",
-  accountController.buildLogin
-);
+router.get("/register", accountController.buildRegister);
 
 module.exports = router;
