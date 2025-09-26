27c7168ef3d51a106e46631f0b67fb1fc2a31006
MichaelKazembe <michaelkazembe@gmail.com>
2025-09-26 22:11:30 +0200

    Enhance UI for login and registration forms; add password requirements and improve button styles

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,12 +2,14 @@
 const express = require("express");
 const router = new express.Router();
 const accountController = require("../controllers/accountController");
-const utilities = require("../utilities/");
 
 // Route to build the login view
 router.get("/login", accountController.buildLogin);
 
 // Route to build the registration view
 router.get("/register", accountController.buildRegister);
+
+// Router to register a new account
+router.post("/register", accountController.registerAccount);
 
 module.exports = router;
