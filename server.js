2ed403c8016d7d9fe1ee15509486d589c4caab0e
MichaelKazembe <michaelkazembe@gmail.com>
2025-09-26 18:00:32 +0200

    Add connect-flash middleware for flash message support

--- server.js
+++ server.js
@@ -41,6 +41,13 @@ app.use(
   })
 );
 
+//  Express Message  Middleware
+app.use(require("connect-flash")());
+app.use(function (req, res, next) {
+  res.locals.messages = require("express-messages")(req, res);
+  next();
+});
+
 /* ***********************
  * Routes
  *************************/
