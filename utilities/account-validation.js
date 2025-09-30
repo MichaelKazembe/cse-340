f7f80b5a081431798fa4404df04229870c4924bb
MichaelKazembe <michaelkazembe@gmail.com>
2025-09-30 13:06:40 +0200

    Add express-validator for registration data validation rules

---
+++ utilities/account-validation.js
@@ -0,0 +1,47 @@
+const utilities = require("../utilities/index");
+const { body, validationResult } = require("express-validator");
+const validate = {};
+
+/* ************************
+ *  Registration Data Validation Rules
+ * ************************ */
+validate.registrationRules = () => {
+  return [
+    // First name is required and must be string
+    body("account_firstname")
+      .trim()
+      .escape()
+      .notEmpty()
+      .isLength({ min: 1 })
+      .withMessage("First name is required."),
+
+    // Last name is required and must be string
+    body("account_lastname")
+      .trim()
+      .escape()
+      .isLength({ min: 2 })
+      .withMessage("Last name is required."),
+
+    // Valid email is required and cannot already exist in database
+    body("account_email")
+      .trim()
+      .escape()
+      .notEmpty()
+      .isEmail()
+      .normalizeEmail() // Normalizes the email address (e.g., removes dots in Gmail addresses)
+      .withMessage("A valid email is required."),
+
+    // password is required and must be strong
+    body("account_password")
+      .trim()
+      .notEmpty()
+      .isStrongPassword({
+        minLength: 12,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })
+      .withMessage("Password does not meet requirements."),
+  ];
+};
