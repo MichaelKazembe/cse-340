30bf4550c21465755e906ccc693882f30feada25
MichaelKazembe <michaelkazembe@gmail.com>
2025-10-06 22:44:20 +0200

    imported utilities to checkLogin

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,5 +1,6 @@
 // Needed Resources
 const regValidate = require("../utilities/account-validation");
+const utilities = require("../utilities/");
 const express = require("express");
 const router = new express.Router();
 const accountController = require("../controllers/accountController");
