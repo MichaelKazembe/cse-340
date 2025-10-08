5e02d8ea7e3f3c89e85ebf5ad58351c7f2554c7b
MichaelKazembe <michaelkazembe@gmail.com>
2025-10-08 22:26:31 +0200

    Built the Controller Function

--- controllers/invController.js
+++ controllers/invController.js
@@ -189,4 +189,20 @@ invCont.handleAddInventory = async function (req, res, next) {
   }
 };
 
+/* ***************************
+ *  Return Inventory by Classification As JSON
+ * ************************** */
+
+invCont.getInventoryJSON = async (req, res, next) => {
+  const classification_id = parseInt(req.params.classification_id);
+  const invData = await invModel.getInventoryByClassificationId(
+    classification_id
+  );
+  if (invData[0].inv_id) {
+    return res.json(invData);
+  } else {
+    next(new Error("No data returned"));
+  }
+};
+
 module.exports = invCont; // Export the controller object to be used in routes
